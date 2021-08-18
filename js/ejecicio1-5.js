8f0bdc7d46f64571e9409b2391c8b42a66ebfcf9
Jonandy <jonandy.js@gmail.com>
2021-08-17 22:56:54 -0300

    se realizo hasta ejercicio 3

---
+++ js/ejecicio1-5.js
@@ -0,0 +1,11 @@
+// Ejercicio 1
+alert('esto es ventana emergente')
+
+// Ejercicio 2
+document.write('HELLO WORLD')
+
+// Ejercicio 3
+
+// a = 3
+// b = 5
+document.write('el resultado es ' + (3 + 5));
