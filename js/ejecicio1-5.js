4b9b114f988e64a61591e190bc1e89b1df7c4170
Jonandy <jonandy.js@gmail.com>
2021-08-19 16:58:22 -0300

    ejercicio 5

--- js/ejecicio1-5.js
+++ js/ejecicio1-5.js
@@ -8,4 +8,15 @@ document.write('HELLO WORLD')
 
 // a = 3
 // b = 5
-document.write('el resultado es ' + (3 + 5));
+document.write('<br>el resultado es ' + (3 + 5));
+
+//Ejercicio 5
+// parseInt('numero') = se transforma a formato entero
+let numero1 = parseInt(prompt('Ingrese un primer numero'));
+let numero2 = parseInt(prompt('Ingrese un segundo numero'));
+
+document.write('<br>El resultado de la suma es' + (numero1+numero2))
+console.log(numero1)
+console.log(numero1 + numero2);
+
+//Ejercicio
