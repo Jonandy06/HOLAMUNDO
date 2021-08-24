176ae63c5df15015f16192f1f74bc05d4249d6b1
Jonandy <jonandy.js@gmail.com>
2021-08-24 16:47:30 -0300

    ejercicios completos hasta el 8

--- js/ejecicio1-5.js
+++ js/ejecicio1-5.js
@@ -1,22 +1,60 @@
 // Ejercicio 1
-alert('esto es ventana emergente')
+// alert('esto es ventana emergente')
 
 // Ejercicio 2
-document.write('HELLO WORLD')
+// document.write('HELLO WORLD')
 
 // Ejercicio 3
 
 // a = 3
 // b = 5
-document.write('<br>el resultado es ' + (3 + 5));
+// document.write('<br>el resultado es ' + (3 + 5));
 
 //Ejercicio 5
 // parseInt('numero') = se transforma a formato entero
-let numero1 = parseInt(prompt('Ingrese un primer numero'));
-let numero2 = parseInt(prompt('Ingrese un segundo numero'));
+// let numero1 = parseInt(prompt('Ingrese un primer numero'));
+// let numero2 = parseInt(prompt('Ingrese un segundo numero'));
 
-document.write('<br>El resultado de la suma es' + (numero1+numero2))
-console.log(numero1)
-console.log(numero1 + numero2);
+// document.write('<br>El resultado de la suma es' + (numero1+numero2))
+// console.log(numero1)
+// console.log(numero1 + numero2);
 
-//Ejercicio
+//Ejercicio 6
+// let contador1 = parseInt(prompt('Ingrese el primer numero'));
+// let contador2 = parseInt(prompt('Ingrese el segundo numero'));
+
+// if (contador1 > contador2) {
+//     document.write('<br>La variable contador1 es mayor ' + contador1);
+// }else if (contador2 > contador1) {
+//     document.write('<br>La variable contador2 es mayor ' + contador2);
+// }
+
+//Ejercicio 7
+// let numero1 = parseInt(prompt('Ingrese un primer numero'));
+// let numero2 = parseInt(prompt('Ingrese un segundo numero'));
+// let numero3 = parseInt(prompt('Ingrese un tercer numero'));
+
+// if (numero1 > numero2 && numero1 > numero3) {
+//     document.write("<br>La numero es mayor " + numero1);
+    
+// }if (numero2 > numero1 && numero2 > numero3) {
+//     document.write("<br>La numero es mayor " + numero2);
+// }
+// if (numero3 > numero1 && numero2) {
+//     document.write("<br>La numero es mayor " + numero3);
+// }
+
+//Ejercicio 8
+// let numero1 = parseInt(prompt('Ingrese un primer numero'));
+
+
+// if (numero1 % 2 === 0) {
+//     document.write("Es numero par ");
+// }
+
+//Ejercicio 9
+
+let frase = prompt('Ingrese una frase ');
+
+let letras = frase.substr(0);
+document.write(letras);
